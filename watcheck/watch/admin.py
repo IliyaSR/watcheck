5780a5fc948d8398584b3fbc487afbfe7ced76c8
IliyaSr <insomniq4@abv.bg>
2024-02-23 11:38:33 +0200

    Add Watch model

--- watcheck/watch/admin.py
+++ watcheck/watch/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from watcheck.watch.models import Watch
+
+
 # Register your models here.
+@admin.register(Watch)
+class WatchAdmin(admin.ModelAdmin):
+    list_display = ['brand', 'model', 'watch_code', 'price']
+    filter = ['brand']
+
+
--- watcheck/watch/models.py
+++ watcheck/watch/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+
 # Create your models here.
+class Watch(models.Model):
+    main_image = models.ImageField()
+    image_for_shop_page = models.ImageField()
+    first_small_image = models.ImageField()
+    second_small_image = models.ImageField()
+    third_small_image = models.ImageField()
+    fourth_small_image = models.ImageField()
+    brand = models.CharField(max_length=30)
+    model = models.CharField(max_length=30)
+    details_text = models.TextField()
+    watch_code = models.CharField(max_length=20)
+    price = models.IntegerField()
+    case_material = models.CharField(max_length=30)
+    strap_material = models.CharField(max_length=30)
+    movement = models.CharField(max_length=30)
+    water_resistance = models.IntegerField()
+    case_diameter = models.FloatField()
+    case_thickness = models.FloatField()
+    glass = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.brand
