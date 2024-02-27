1478926cc0c5eb02ba2ed694c623bfd86ca7b366
IliyaSr <insomniq4@abv.bg>
2024-02-27 23:42:34 +0200

    Add filter by price

--- watcheck/watch/views.py
+++ watcheck/watch/views.py
@@ -1,3 +1,6 @@
+from itertools import chain
+
+from django.db.models import Q
 from django.shortcuts import render
 
 from watcheck.watch.models import Watch
@@ -15,10 +18,27 @@ def shop(request):
 
 def filter_watches(request):
     brand = request.GET.getlist('brand')
+    price = request.GET.getlist('price')
     watches = Watch.objects.all()
+
+    if len(price) == 1:
+        current_price = [int(watch_price) for watch_price in price[0].split(', ')]
+    else:
+        current_price = []
+        for index in range(len(price)):
+            current_price.append([int(watch_price) for watch_price in price[index].split(', ')])
 
     if brand:
         watches = watches.filter(brand__in=brand)
+    if price:
+        if len(price) == 1:
+            watches = watches.filter(price__range=current_price)
+        else:
+            result = []
+            for price in current_price:
+                result.append(watches.filter(price__range=price))
+
+            watches = list(chain(*result))
 
     return render(request, template_name='watch/shop.html', context={"watches": watches})
 
